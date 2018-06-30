acdc0b39a9c835face2c4094e0ff44fe150930fd
TARUN GOYAL <goyaltarun584@gmail.com>
2018-06-30 12:30:47 +0530

    Splash Activity added

--- app/src/main/java/com/example/mypc/firstskool/HomeActivity.java
+++ app/src/main/java/com/example/mypc/firstskool/HomeActivity.java
@@ -9,5 +9,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
