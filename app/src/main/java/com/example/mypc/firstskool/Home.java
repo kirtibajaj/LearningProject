d103debea21ef06307499d294544dbc9a2dade6b
kirti <kirti.bajaj93@gmail.com>
2018-07-03 02:22:53 +0530

    Home Page Started

--- app/src/main/java/com/example/mypc/firstskool/Home.java
+++ app/src/main/java/com/example/mypc/firstskool/Home.java
@@ -1,8 +1,13 @@
 package com.example.mypc.firstskool;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,8 +27,34 @@ public class Home extends AppCompatActivity
         setContentView(R.layout.activity_home2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+           //Code of Bottom Navigation
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+                = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                   /* case R.id.profile:
+                        setfragment();
+                        return true;
+                    case R.id.library:
+                        setfragment();
+                        return true;
+                    case R.id.challenge:
+                        setfragment();
+                        return true;
+                    case R.id.chatbot:
+                        setfragment();
+                        return true;
+                    case R.id.home:
+                        setfragment();
+                        return true;
+               */ }
+                return false;
+            }
+        };
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -54,9 +85,6 @@ public class Home extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -73,22 +101,50 @@ public class Home extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
+            // Handle the home action
+        } else if (id == R.id.syllabus) {
+            //HAndle Syllabus action
+        } else if (id == R.id.plan) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.bookmark) {
 
-        } else if (id == R.id.nav_manage) {
+        }
+        else if (id == R.id.order) {
+
+        }
+        else if (id == R.id.leaderboard) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        }
+        else if (id == R.id.rateus) {
 
         }
+        else if (id == R.id.likeus) {
+
+        }
+        else if (id == R.id.settings) {
+
+        }
+        else if (id == R.id.logout) {
+
+        }
+
+
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    private void setfragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
+
     }
 }
