04145d511061d8b4a824124bbd568842846791bd
Manish <manishsencha1997@gmail.com>
2018-07-06 20:07:23 +0530

    Action bar added

--- app/src/main/java/com/example/mypc/firstskool/Mycart.java
+++ app/src/main/java/com/example/mypc/firstskool/Mycart.java
@@ -3,11 +3,11 @@ package com.example.mypc.firstskool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Notification extends AppCompatActivity {
-    // class file for notification at action bar
+public class Mycart extends AppCompatActivity {
+    // Activity class for mycart icon at action bar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification);
+        setContentView(R.layout.activity_mycart);
     }
 }
