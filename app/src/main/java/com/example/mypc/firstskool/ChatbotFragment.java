457a8aca57ec84783ec5dd88769d3448cde32c24
Manish <manishsencha1997@gmail.com>
2018-07-08 13:34:14 +0530

    Chatbotadd

---
+++ app/src/main/java/com/example/mypc/firstskool/ChatbotFragment.java
@@ -0,0 +1,21 @@
+package com.example.mypc.firstskool;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by CSC on 7/6/2018.
+ */
+
+public class ChatbotFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.chatbot_fragment, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/mypc/firstskool/Home.java
@@ -0,0 +1,169 @@
+package com.example.mypc.firstskool;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Home extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+           //Code of Bottom Navigation
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+                = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.profile:
+                        //setfragment();
+                        return true;
+                    case R.id.library:
+                        //setfragment();
+                        return true;
+                    case R.id.challenge:
+                        //setfragment();
+                        return true;
+                    case R.id.chatbot:
+                        ChatbotFragment chatbotFragment=new ChatbotFragment();
+                        setfragment(chatbotFragment);
+                        return true;
+                    case R.id.home:
+                        //setfragment();
+                        return true;
+                }
+                return false;
+            }
+        };
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        // notification at action bar
+        if (id == R.id.notification) {
+            openNotification();
+            return true;
+        }
+        else if(id == R.id.cart){
+            myCart();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void myCart(){
+        Intent intent=new Intent(this,Mycart.class);
+        startActivity(intent);
+    }
+    public void openNotification(){
+        Intent intent=new Intent(this,Notification.class);
+        startActivity(intent);
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the home action
+        } else if (id == R.id.syllabus) {
+            //HAndle Syllabus action
+        } else if (id == R.id.plan) {
+
+        } else if (id == R.id.bookmark) {
+
+        }
+        else if (id == R.id.order) {
+
+        }
+        else if (id == R.id.leaderboard) {
+
+        } else if (id == R.id.nav_share) {
+
+        }
+        else if (id == R.id.rateus) {
+
+        }
+        else if (id == R.id.likeus) {
+
+        }
+        else if (id == R.id.settings) {
+
+        }
+        else if (id == R.id.logout) {
+
+        }
+
+
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    private void setfragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
+
+    }
+}
